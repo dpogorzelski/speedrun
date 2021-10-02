4022c7d2f438fd96de2668441c8eb88e74b85116
Dawid Pogorzelski <dawid@pogorzelski.dev>
2021-10-02 23:28:50 +0200

    wip

--- pkg/speedrun/cloud/google.go
+++ pkg/speedrun/cloud/google.go
@@ -23,3 +23,30 @@ func NewGCPClient() (*GoogleClient, error) {
 
 	return &GoogleClient{gce}, nil
 }
+
+// GetInstances returns a list of external IP addresses used for the SHH connection
+func (c *GoogleClient) GetInstances(project, filter string) ([]Instance, error) {
+	instances := []Instance{}
+	listCall := c.Instances.AggregatedList(project).Fields("nextPageToken", "items(Name,NetworkInterfaces)")
+	var ctx context.Context
+
+	listCall.Filter(filter).Pages(ctx, func(list *compute.InstanceAggregatedList) error {
+		for _, item := range list.Items {
+			for _, instance := range item.Instances {
+				i := Instance{
+					Name:           instance.Name,
+					PrivateAddress: instance.NetworkInterfaces[0].NetworkIP,
+					PublicAddress:  instance.NetworkInterfaces[0].AccessConfigs[0].NatIP,
+				}
+				instances = append(instances, i)
+			}
+		}
+		return nil
+	})
+	_, err := listCall.Do()
+	if err != nil {
+		return nil, err
+	}
+
+	return instances, nil
+}
