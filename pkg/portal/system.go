c9ba91c585f7bc730fc8de1bb1e66cd78d20774f
Dawid Pogorzelski <dawid@pogorzelski.dev>
2022-05-13 23:06:51 +0200

    Add the system command with reboot subcommand

---
+++ pkg/portal/system.go
@@ -0,0 +1,25 @@
+//go:build linux && amd64
+
+package portal
+
+import (
+	"context"
+	"syscall"
+
+	"github.com/apex/log"
+	"github.com/dpogorzelski/speedrun/proto/portal"
+)
+
+func (s *Server) SystemReboot(ctx context.Context, file *portal.SystemRebootRequest) (*portal.SystemRebootResponse, error) {
+	fields := log.Fields{
+		"context": "system",
+		"command": "reboot",
+	}
+	log := log.WithFields(fields)
+	log.Debug("Received system reboot request")
+
+	syscall.Sync()
+	go syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART)
+
+	return &portal.SystemRebootResponse{State: portal.State_CHANGED, Message: "Rebooting"}, nil
+}
